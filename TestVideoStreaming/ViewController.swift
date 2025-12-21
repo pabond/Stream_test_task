8553ebe73f444bbb9cf8c2ae2155a136e8324dd3
Pavel Bondar <pavlo.condar@crello.com>
2025-12-21 18:39:19 +0100

    project initialization

---
+++ TestVideoStreaming/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestVideoStreaming
+//
+//  Created by Pavel Bondar on 21.12.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestVideoStreamingTests/TestVideoStreamingTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestVideoStreamingTests.swift
+//  TestVideoStreamingTests
+//
+//  Created by Pavel Bondar on 21.12.25.
+//
+
+import Testing
+@testable import TestVideoStreaming
+
+struct TestVideoStreamingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
