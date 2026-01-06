2ad2fc68e9622a0f4721fb0d7c62881d34edcb59
Pavel Bondar <pavlo.condar@crello.com>
2026-01-06 20:48:53 +0100

    add constants

--- TestVideoStreaming/Resources/Constants.swift
+++ TestVideoStreaming/Resources/Constants.swift
@@ -13,4 +13,14 @@ struct Constants {
     static let buttonHeight: CGFloat = 44
     static let buttonFontSize: CGFloat = 18
     static let buttonCornerRadius: CGFloat = 8
+    
+    struct IconsNames {
+        static let mute = "mic.slash.fill"
+        static let mic = "mic.fill"
+        static let play = "play.fill"
+        static let stop = "stop.fill"
+        static let switchCamera = "camera.rotate.fill"
+        static let error = "exclamationmark.triangle.fill"
+        static let connecting = "arrow.2.circlepath.circle"
+    }
 }
